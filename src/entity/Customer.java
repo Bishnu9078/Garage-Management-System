1fe14278437cac97b99e446352b3dd78ff51314a
bishnu <bishnuprasadsahoo068@gmail.com>
2025-12-02 17:56:42 +0530

    config & DBConnection

---
+++ src/entity/Customer.java
@@ -0,0 +1,16 @@
+package entity;
+
+public class Customer {
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    private int id;
+
+    private String name;
+
+    private String phone;
+
+
+}
