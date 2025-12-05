4dd85a3193207309edc8c3dad8e09dbcaf44a5b4
bishnu <bishnuprasadsahoo068@gmail.com>
2025-12-05 20:13:35 +0530

    adding somthing

--- src/config/DBConfig.java
+++ src/config/DBConfig.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConfig {
 
     private static final String URL = "jdbc:mysql://localhost:3306/garage";
-    private static finl String USER = "root";
+    private static final String USER = "root";
     private static final String PASS = "Bishnu@068";
 
 
